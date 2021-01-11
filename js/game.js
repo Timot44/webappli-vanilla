756cf561a4b94f15f0736fd880fe1a01acddbc9a
Timothé <rondeautimothe@gmail.com>
2021-01-11 10:29:40 +0100

    red_block

--- js/game.js
+++ js/game.js
@@ -2,11 +2,17 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 100, 100);
-
+let x = 10
+let y = 10
 function gameLoop(){
 
-    console.log("Hello !")
+    ctx.fillStyle = 'red'
+    ctx.fillRect(x,10,100,100)
+    x += 0.5
+
+
+    ctx.fillStyle = "black"
+    ctx.fillRect(y,10,200,200)
+    y+= 0.5
 }
-setInterval(gameLoop, 1000)
+setInterval(gameLoop, 1000/60)
